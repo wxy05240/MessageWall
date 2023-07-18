51899071fb4a9dbf8cf9966a8884dcf19631fa91
wxy05240 <782268618@qq.com>
2023-07-18 17:22:01 +0800

    更新部分

--- Web/src/assets/utils/env.js
+++ Web/src/assets/utils/env.js
@@ -3,8 +3,8 @@ let routerMode = 'hash'
 let avatarUrl = ''
 let baseImgPath
 if(process.env.NODE_ENV == 'development'){
-    baseUrl = 'https://localhost:3000/'
-    baseImgPath = 'https://localhost:3000'
+    baseUrl = 'http://localhost:3000/walls'
+    baseImgPath = 'http://localhost:3000'
     avatarUrl = 'http://img.5i21.cn/LightPicture/2022/10/eeaaaa6df26aefe5.jpg'
 }else{
     // 此处只需要修改api.5i21.cn为你的后端地址
